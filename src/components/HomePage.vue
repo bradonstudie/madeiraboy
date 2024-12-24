186dcd1d9a75625addc0c2b8a5f57f2a59ab42b8
Bradon Studebaker <bradon@taivara.com>
2024-12-23 22:08:36 -0500

    initial UI setup

---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+            <img :src="bunnyUrl" class="bunny-logo" />
+        </div>
+        <div class="col-12">
+            <h1 class="text-center">madeiraboy.com</h1>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import bunnyUrl from '../assets/bunny.png';
+</script>
+
+<style scoped>
+.bunny-logo {
+    width: 10rem;
+}
+</style>
